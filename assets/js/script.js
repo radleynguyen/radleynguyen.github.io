4b61943c8c11becd1e5e83d2c5adf309926696e9
Radley Nguyen <nguyen41292@gmail.com>
2026-01-24 04:38:33 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-
+function sendMail(){
+  let parms = {
+      name : document.getElementById("name").value,
+      email : document.getElementById("email").value,
+      subject : document.getElementById("subject").value,
+      message : document.getElementById("message").value,
+  }
+  emailjs.send("service_rn9gh5f","template_oji7izx",parms).then(alert("Email Sent!!"))
+}

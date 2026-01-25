a6ca40d79eebdb7d779ea4a41d8d211538badeb4
Radley Nguyen <nguyen41292@gmail.com>
2026-01-25 15:33:07 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,31 @@
-function sendMail(event){
-  event.preventDefault(); // CHẶN reload
+(function () {
+  emailjs.init({
+    publicKey: "Y_0W95wc9RwPzpgsg",
+  });
+})();
 
-  let parms = {
-    name : document.getElementById("name").value,
-    email : document.getElementById("email").value,
-    message : document.getElementById("message").value,
-  };
+window.onload = function () {
+  const form = document.querySelector("contact-form");
 
-  emailjs.send("service_rn9gh5f", "template_oji7izx", parms)
-    .then(() => alert("Email Sent!!"))
-    .catch(err => alert("Error: " + err));
-}
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // chặn reload
+
+    const templateParams = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      subject: document.getElementById("subject").value,
+      message: document.getElementById("message").value,
+    };
+
+    emailjs
+      .send("service_rn9gh5f", "template_oji7izx", templateParams)
+      .then(() => {
+        alert("✅ Email sent successfully!");
+        form.reset();
+      })
+      .catch((error) => {
+        alert("❌ Failed to send email");
+        console.error("EmailJS error:", error);
+      });
+  });
+};
